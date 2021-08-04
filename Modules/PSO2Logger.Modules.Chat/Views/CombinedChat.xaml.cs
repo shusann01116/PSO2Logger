d7f10eac96c3da0accd06302fba5ab4754593d8e
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2021-08-05 00:43:04 +0900

    やっと改行表示に対応、うれぴ

---
+++ Modules/PSO2Logger.Modules.Chat/Views/CombinedChat.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace PSO2Logger.Modules.Chat.Views {
+    /// <summary>
+    /// Interaction logic for CombinedChat
+    /// </summary>
+    public partial class CombinedChat : UserControl {
+        public CombinedChat() {
+            InitializeComponent();
+        }
+    }
+}
