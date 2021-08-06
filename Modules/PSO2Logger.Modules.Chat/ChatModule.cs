f7650b6aa1a23f4f31fbf124411fa20502ba7d39
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2021-08-06 22:30:14 +0900

    ChatBodyを起動時から表示できるように変更

--- Modules/PSO2Logger.Modules.Chat/ChatModule.cs
+++ Modules/PSO2Logger.Modules.Chat/ChatModule.cs
@@ -1,11 +1,14 @@
 ﻿using Prism.Ioc;
 using Prism.Modularity;
+using Prism.Regions;
+using PSO2Logger.Core;
 using PSO2Logger.Modules.Chat.Views;
 
 namespace PSO2Logger.Modules.Chat {
     public class ChatModule : IModule {
         public void OnInitialized(IContainerProvider containerProvider) {
-
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion<ChatBody>(RegionName.ContentRegion);
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry) {
