6fd35102655cc473025bbb26c367bbb079c4d232
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2021-08-07 16:10:15 +0900

    Added Color properties

---
+++ Modules/PSO2Logger.Modules.Chat/Core/DefaultChatColor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSO2Logger.Modules.Chat.Core {
+    static class DefaultChatColor {
+        readonly static string PublicChat = "White";
+        readonly static string PartyChat = "Blue";
+        readonly static string TeamChat = "Yellow";
+        readonly static string WhisperChat = "Cyan";
+        readonly static string GroupChat = "Green";
+    }
+}
