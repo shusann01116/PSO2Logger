2e94900c882f0ec41c2473095837fc7098381ed4
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2021-08-07 16:10:15 +0900

    Added Color properties

--- Modules/PSO2Logger.Modules.Chat/ViewModels/CombinedChatViewModel.cs
+++ Modules/PSO2Logger.Modules.Chat/ViewModels/CombinedChatViewModel.cs
@@ -13,7 +13,37 @@ namespace PSO2Logger.Modules.Chat.ViewModels {
         public ObservableCollection<ChatLine> PartyChats { get; set; } 
         public ObservableCollection<ChatLine> TeamChats { get; set; }
         public ObservableCollection<ChatLine> WhisperChats { get; set; } 
-        public ObservableCollection<ChatLine> GroupChats { get; set; } 
+        public ObservableCollection<ChatLine> GroupChats { get; set; }
+
+        private string _publicChatColor;
+        public string PublicChatColor {
+            get { return _publicChatColor; }
+            set { SetProperty(ref _publicChatColor, value); }
+        }
+
+        private string _partyChatColor;
+        public string PartyChatColor {
+            get { return _partyChatColor; }
+            set { SetProperty(ref _partyChatColor, value); }
+        }
+
+        private string _teamChatColor;
+        public string TeamChatColor {
+            get { return _teamChatColor; }
+            set { SetProperty(ref _teamChatColor, value); }
+        }
+
+        private string _whisperChatColor;
+        public string WhisperChatColor {
+            get { return _whisperChatColor; }
+            set { SetProperty(ref _whisperChatColor, value); }
+        }
+
+        private string _groupChatColor;
+        public string GroupChatColor {
+            get { return _groupChatColor; }
+            set { SetProperty(ref _groupChatColor, value); }
+        }
 
         public CombinedChatViewModel(ILogService<ChatLine> logService, IWatcherService watcherService) {
             chatDataStore = new ChatDataStore(logService, watcherService);
