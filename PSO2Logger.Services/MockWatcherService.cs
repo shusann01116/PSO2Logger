d1b0228bfe970c72890902953401fab620e3a968
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2021-08-07 14:26:12 +0900

    FileWatcherServiceのパフォーマンスが悪すぎたのでMockWatcherServiceに差し替え

---
+++ PSO2Logger.Services/MockWatcherService.cs
@@ -0,0 +1,26 @@
+﻿using PSO2Logger.Interfaces;
+using PSO2Logger.Interfaces.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PSO2Logger.Services {
+    public class MockWatcherService : IWatcherService {
+        public string FolderPath { get; set; }
+        public string FileName { get; set; }
+        public bool CanRaiseEvent { get; set; } = true;
+
+        public event EventHandler<WatcherEventArgs> OnChanged;
+
+        public void RunScanRoop() {
+            var watcherEventArgs = new WatcherEventArgs(null, WatchType.FileChanged);
+            while (CanRaiseEvent is true) {
+                Thread.Sleep(1000);
+                OnChanged?.Invoke(this, watcherEventArgs);
+            }
+        }
+    }
+}
