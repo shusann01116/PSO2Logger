279e9e91d0a858731e5128dac530f8e95337962d
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2021-08-10 00:26:10 +0900

    Added IsShowCommand parameter to view

--- Modules/PSO2Logger.Modules.Settings/ViewModels/SettingViewModel.cs
+++ Modules/PSO2Logger.Modules.Settings/ViewModels/SettingViewModel.cs
@@ -1,5 +1,7 @@
-﻿using Prism.Commands;
+﻿using Microsoft.Extensions.Configuration;
+using Prism.Commands;
 using Prism.Mvvm;
+using PSO2Logger.Core;
 using PSO2Logger.Interfaces;
 using PSO2Logger.Models;
 using System;
@@ -9,6 +11,7 @@ using System.Linq;
 
 namespace PSO2Logger.Modules.Settings.ViewModels {
     public class SettingViewModel : BindableBase {
+        private readonly IConfigurationRoot configuration;
         private readonly ILogService<ChatLine> logService;
         private readonly IWatcherService watcherService;
 
@@ -22,11 +25,19 @@ namespace PSO2Logger.Modules.Settings.ViewModels {
             }
         }
 
-        public SettingViewModel(ILogService<ChatLine> logService, IWatcherService watcherService) {
+        private bool _showCommand;
+        public bool IsShowCommand {
+            get { return _showCommand; }
+            set { SetProperty(ref _showCommand, value); }
+        }
+
+        public SettingViewModel(IConfigurationRoot configuration, ILogService<ChatLine> logService, IWatcherService watcherService) {
+            this.configuration = configuration;
             this.logService = logService;
             this.watcherService = watcherService;
 
             FolderPath = logService.FolderPath;
+            IsShowCommand = bool.Parse(configuration.GetSection(SettingKeys.IsShowCommand).Value);
         }
 
         private void UpdateServiceFolderPath(string folderPath) {
